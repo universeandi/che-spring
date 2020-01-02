83b8a6c12f961c09ea4d73a10c23a8cfbd683775
universeandi <universeandi>
2020-01-02 10:45:51 +0900

    Base project

---
+++ che/src/main/java/com/eclipse/che/CheApplication.java
@@ -0,0 +1,13 @@
+package com.eclipse.che;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheApplication.class, args);
+	}
+
+}
---
+++ che/src/test/java/com/eclipse/che/CheApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eclipse.che;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CheApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
